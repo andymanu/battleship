8e445815518de5e6b740703f3e70a0296046ed61
kushani41 <kushani41@gmail.com>
2016-04-23 00:38:41 +1000

    fixing high score

--- C-sharp-Version/src/UtilityFunctions.cs
+++ C-sharp-Version/src/UtilityFunctions.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 //using System.Data;
 using System.Diagnostics;
 using SwinGameSDK;
+
 /// <summary>
 /// This includes a number of utility methods for
 /// drawing and interacting with the Mouse.
@@ -134,9 +135,9 @@ static class UtilityFunctions
 				draw = true;
 
 				switch (grid[row, col]) {
-					//case TileView.Ship:
-					//	draw = false;
-					//	break;
+					case TileView.Ship:
+						draw = false;
+						break;
 					//If small Then fillColor = _SMALL_SHIP Else fillColor = _LARGE_SHIP
 					case TileView.Miss:
 						if (small)
@@ -151,12 +152,17 @@ static class UtilityFunctions
 							fillColor = LARGE_HIT;
 						break;
 					case TileView.Sea:
-					case TileView.Ship:
 						if (small)
 							fillColor = SMALL_SEA;
 						else
 							draw = false;
 						break;
+					//case TileView.Ship:
+					//	if (small)
+					//		fillColor = SMALL_SEA;
+					//	else
+					//		draw = false;
+					//	break;
 				}
 
 				if (draw) {
@@ -253,7 +259,7 @@ static class UtilityFunctions
 
 	public static void AddExplosion(int row, int col)
 	{
-		AddAnimation(row, col, "Splash");
+		AddAnimation(row, col, "Explosion");
 	}
 
 	public static void AddSplash(int row, int col)
